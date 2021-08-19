6688c168bb3d0ac7e7c13300904e596432fa2867
Abhishek N <nabhishek865@gmail.com>
2021-08-19 21:40:32 +0530

    sec

--- calculator.js
+++ calculator.js
@@ -1,14 +1,21 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-    res.send("Thanks");
+    var number1 = req.body.number1;
+    var number2 = req.body.number2;
+    var result = number1+number2;
+
+    res.send("result of the request is "+result);
 });
 
 app.listen(3000,function () {
