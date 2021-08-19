4a785c9f45bf97f18b4fad39a315f17349eea73d
Abhishek N <nabhishek865@gmail.com>
2021-08-19 21:56:30 +0530

    sec

--- calculator.js
+++ calculator.js
@@ -11,13 +11,23 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    var number1 = req.body.number1;
-    var number2 = req.body.number2;
+    var number1 = Number(req.body.number1);
+    var number2 = Number(req.body.number2);
     var result = number1+number2;
 
     res.send("result of the request is "+result);
 });
 
+
+app.get("bmicalculator",function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("bmicalculator",function (req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight/(height*height);
+    res.send("your bmi is " +bmi);
+});
 app.listen(3000,function () {
     console.log("listening on port 3000");
 });
